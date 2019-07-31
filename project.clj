33d3727d142de5e6941855c47eb925a195d657b4
Tuomo Virolainen <tuomo.virolainen@siili.com>
2019-07-31 20:37:36 +0300

    Tweak deployment settings once again

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :aliases {"analyze" ["eastwood" "{:linters [:unused-namespaces]}"]}
   :plugins [[jonase/eastwood "0.3.5"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env
-                                     :password :env
+                                     :username :env/clojars_user
+                                     :password :env/clojars_pass
                                      :sign-releases false}]]
   :repl-options {:init-ns blurhash.core})
