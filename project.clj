0e696f9418481fd9e7c253d937b488c50e08e2fd
Tuomo Virolainen <tuomo.virolainen@siili.com>
2019-08-05 22:39:53 +0300

    Deployment, again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject siili-core/blurhash "0.0.1"
+(defproject blurhash "0.0.1"
   :description "A Clojure(Script) implementation of the blurhash algorithm"
   :url "http://github.com/siili-core/blurhash"
   :license {:name "MIT License"
@@ -6,16 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :aliases {"analyze" ["eastwood" "{:linters [:unused-namespaces]}"]}
   :plugins [[jonase/eastwood "0.3.5"]]
-  :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"] ; notice that the map is not quoted.
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
-                                  :username :env/clojars_username
-                                  :password :env/clojars_password
-                                  :sign-releases false}]]
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "v" "--no-sign"]
-                  ["deploy"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]]
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]]
   :repl-options {:init-ns blurhash.core})
