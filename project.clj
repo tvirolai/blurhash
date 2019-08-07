df0cc6059850c2bb5483171580df7ee3332645b2
Tuomo Virolainen <tuomo.virolainen@siili.com>
2019-08-07 14:41:55 +0300

    Tweak the deployment settings once more

--- project.clj
+++ project.clj
@@ -1,13 +1,22 @@
-(defproject siili-core/blurhash "0.0.1"
+(defproject siili-core/blurhash "0.0.2"
   :description "A Clojure(Script) implementation of the blurhash algorithm"
   :url "http://github.com/siili-core/blurhash"
   :license {:name "MIT License"
             :url "https://github.com/siili-core/blurhash/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :aliases {"analyze" ["eastwood" "{:linters [:unused-namespaces]}"]}
-  :plugins [[jonase/eastwood "0.3.5"]]
+  :plugins [[jonase/eastwood "0.3.5"]
+            [lein-shell "0.5.0"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
                                     :sign-releases false}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["shell" "git" "commit" "-am" "Version ${:version} [ci skip]"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["shell" "git" "commit" "-am" "Version ${:version} [ci skip]"]
+                  ["vcs" "push"]]
   :repl-options {:init-ns blurhash.core})
